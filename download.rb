da84f352c7ec7d217453bcc416f5db2231ae807a
Marc Morata <marc.morata.fite@gmail.com>
2016-10-30 18:26:40 +0100

    add client-id

--- download.rb
+++ download.rb
@@ -6,13 +6,15 @@ if ARGV.length != 1
   exit
 end
 
+client_id = ""
+
 url = ARGV[0]
 
 puts "Downloading #{url}"
 
 vod_id = url.split("/")[-1]
 
-token = JSON.parse(RestClient.get("https://api.twitch.tv/api/vods/#{vod_id}/access_token?as3=t"))
+token = JSON.parse(RestClient.get("https://api.twitch.tv/api/vods/#{vod_id}/access_token?as3=t", :client_id => client_id))
 playlist = RestClient.get("http://usher.justin.tv/vod/#{vod_id}?nauthsig=#{token["sig"]}&nauth=#{token["token"]}")
 
 vid_list_url = playlist.split("\n").select{|l| l.start_with? "http"}[0]
